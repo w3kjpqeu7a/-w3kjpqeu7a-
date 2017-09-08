b573b84fdd9edc2d6db767bb8236f4fedd2721cd
w3kjpqeu7a <31779195+w3kjpqeu7a@users.noreply.github.com>
2017-09-08 15:02:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://w3kjpqeu7a:w3kjpqeu7a@ds129004.mlab.com:29004/w3kjpqeu7a";
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    db.w3kjpqeu7a("customers").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  }); 
+  
   response.send('Hello World!');
 })
 

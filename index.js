28934b258e0d7c2ed1ac6ad8479699412fbba02e
w3kjpqeu7a <31779195+w3kjpqeu7a@users.noreply.github.com>
2017-09-08 15:10:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb://w3kjpqeu7a:w3kjpqeu7a@ds129004.mlab.com:29004/w3kjpqeu7a";
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://w3kjpqeu7a:w3kjpqeu7a@ds129004.mlab.com:29004/w3kjpqeu7a');
 
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var myobj = { name: "Company Inc", address: "Highway 37" };
-    db.w3kjpqeu7a("customers").insertOne(myobj, function(err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-    });
-  }); 
+
+app.get('/', function(request, response) {
+
+ 
+  var Cat = mongoose.model('Cat', { name: String });
+
+  var kitty = new Cat({ name: 'Zildjian' });
+  kitty.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('meow');
+    }
+  });
   
   response.send('Hello World!');
 })
